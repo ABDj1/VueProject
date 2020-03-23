acb9ea290c35e8f8d545df50bfa02f13416aec05
Nicu <nicu@192-168-0-117.rdsnet.ro>
2020-03-23 13:16:12 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './registerServiceWorker'
-import router from './router'
+import router from './router/index'
+import dateFilter from "./filters/date.filters";
 import store from './store'
+import 'materialize-css/dist/js/materialize.min'
+
 
 Vue.config.productionTip = false
+
+Vue.filter('date', dateFilter)
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,64 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
 
-Vue.use(VueRouter)
-
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
-
-const router = new VueRouter({
+Vue.use(Router)
+export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      meta: {layout: 'main'},
+      component: () => import('../views/Home')
+    },
+    {
+      path: '/login',
+      name: 'login',
+      meta: {layout: 'empty'},
+      component: () => import('../views/Login')
+    },
+    {
+      path: '/categories',
+      name: 'categories',
+      meta: {layout: 'main'},
+      component: () => import('../views/Categories')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      meta: {layout: 'empty'},
+      component: () => import('../views/Register')
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      meta: {layout: 'main'},
+      component: () => import('../views/Profile')
+    },
+    {
+      path: '/record',
+      name: 'record',
+      meta: {layout: 'main'},
+      component: () => import('../views/Record')
+    },
+    {
+      path: '/planning',
+      name: 'planning',
+      meta: {layout: 'main'},
+      component: () => import('../views/Planning')
+    },
+    {
+      path: '/history',
+      name: 'history',
+      meta: {layout: 'main'},
+      component: () => import('../views/History')
+    },
+    {
+      path: '/detail',
+      name: 'detail',
+      meta: {layout: 'main'},
+      component: () => import('../views/DetailRecord')
+    }
+  ]
 })
-
-export default router
